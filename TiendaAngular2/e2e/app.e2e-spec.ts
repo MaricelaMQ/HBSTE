ce891d9d0d8c736b0b048bf162c49b101eb7156b
Maricela <lmendez@adia.com.co>
2018-08-29 13:15:13 -0500

    Initial commit

---
+++ TiendaAngular2/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('herramientas_build_tecnologias_emergentes_mm', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
---
+++ TiendaReact/README.md
@@ -0,0 +1,9 @@
+Para poder probar esta version primero deben abrir mongod y ejecutar los siguientes scripts:
+
+1. node server/iniciarData.js
+3. npm run dist
+4. node servernode.js
+
+El usuario inicial es:
+User: user1@tiendanextu.com
+password: Cl@veUser1
